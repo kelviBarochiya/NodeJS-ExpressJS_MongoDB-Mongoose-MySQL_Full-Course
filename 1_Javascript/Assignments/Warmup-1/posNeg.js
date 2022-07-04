3bb075a21f359e2fd760a1a0fb8db9a2ae956b62
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-04 13:18:37 +0530

    warmup-1 solved

---
+++ 1_Javascript/Assignments/Warmup-1/posNeg.js
@@ -0,0 +1,15 @@
+function posNeg(a, b, negative){
+    if(a<0 && b>0 && negative==false){
+      return true;
+    }
+    
+    else if(a>0 && b<0 && negative==false){
+      return true;
+    }
+    else if(a<0 && b<0 && negative==true){
+      return true;
+    }
+    else {
+      return false;
+    }  
+  }
