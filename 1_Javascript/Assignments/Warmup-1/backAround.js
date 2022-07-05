8dfde2cda7e3706c287c4b7f147adc685474ec19
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-05 15:42:55 +0530

    warmup-1 solved

--- 1_Javascript/Assignments/Warmup-1/backAround.js
+++ 1_Javascript/Assignments/Warmup-1/backAround.js
@@ -1,6 +1,5 @@
-function backAround(str){
-    last = str.substring(str.length - 1);
-  
-    return last + str + last;
-  }
-  
+function backAround(str) {
+  end = str.substring(str.length - 1);
+
+  return last + str + end;
+}
