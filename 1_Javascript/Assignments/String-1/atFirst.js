5d55859966dab01620761f6e1dbd80fb78b8c88e
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-06 20:43:27 +0530

    string-1 solved

---
+++ 1_Javascript/Assignments/String-1/atFirst.js
@@ -0,0 +1,11 @@
+function atFirst(str){
+    if (str.length == 0) {
+      return '@@';
+    }
+  
+    if (str.length == 1) {
+      return `${str}@`;
+    }
+  
+    return str.substring(0, 2);
+  }
