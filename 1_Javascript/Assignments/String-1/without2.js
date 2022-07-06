8e72634ca9f0d8acee61de66cc3d321645a4f025
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-06 20:52:52 +0530

    string-1 solved

---
+++ 1_Javascript/Assignments/String-1/without2.js
@@ -0,0 +1,8 @@
+function without2(str){
+    if (str.length >= 2 &&
+          str.substring(0, 2) == str.substring(str.length - 2)) {
+      return str.substring(2);
+    }
+  
+    return str;
+  }
