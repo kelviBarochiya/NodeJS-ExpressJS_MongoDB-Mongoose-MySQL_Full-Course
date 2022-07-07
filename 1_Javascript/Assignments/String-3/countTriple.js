a8e881ac9893593208ee9ed7fbb08b61953cdac3
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-07 20:49:31 +0530

    string-3 solved

---
+++ 1_Javascript/Assignments/String-3/countTriple.js
@@ -0,0 +1,13 @@
+function countTriple(str){
+    count = 0;
+    for(i=0;i<str.length;i++)
+    {
+        if(str[i] == str[i-1] && str[i] == str[i+1])
+        {
+            count++;
+        }
+    }
+    return count;
+
+
+}
