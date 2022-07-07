c209d6d0f3d5e8ac61913b46dc820d47154f8bcd
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-07 23:27:10 +0530

    logic-1 solved

---
+++ 1_Javascript/Assignments/Logic-1/redTicket.js
@@ -0,0 +1,15 @@
+function redTicket(a, b, c){
+    if (a == 2 && b == 2 && c == 2) {
+        return 10;
+      }
+    
+      if (a == b && b == c && a == c) {
+        return 5;
+      }
+    
+      if (b != a && c != a) {
+        return 1;
+      }
+    
+      return 0;
+  }
