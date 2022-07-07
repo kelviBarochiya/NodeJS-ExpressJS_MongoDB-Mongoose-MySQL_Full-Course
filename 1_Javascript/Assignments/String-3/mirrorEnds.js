45edfc8f252f0cf0ccfcdf3c5e35c823d5a8fe3f
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-07 21:02:10 +0530

    string-3 solved

---
+++ 1_Javascript/Assignments/String-3/mirrorEnds.js
@@ -0,0 +1,13 @@
+function mirrorEnds(string){
+    let result = "";
+  
+      for (let i = 0; i < string.length; i++) {
+        if (string[i] == string[string.length - i - 1]) {
+          result += string[i]
+        } else {
+          break;
+        }
+      }
+  
+      return result;
+  }
