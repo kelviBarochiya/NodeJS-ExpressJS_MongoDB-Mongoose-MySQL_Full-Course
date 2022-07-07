5160ed642fc13da40f2102cc734d5215e3d799f9
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-07 22:49:08 +0530

    logic-1 solved

---
+++ 1_Javascript/Assignments/Logic-1/squirrelPlay.js
@@ -0,0 +1,11 @@
+function squirrelPlay(temp, isSummer){
+    if (isSummer && temp >= 60 && temp <= 100) {
+        return true;
+      }
+    
+      if (!isSummer && temp >= 60 && temp <= 90) {
+        return true;
+      }
+    
+      return false;
+  }
