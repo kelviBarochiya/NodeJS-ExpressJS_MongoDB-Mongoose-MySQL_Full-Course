e68866e4dd0e41a72885e03c8947d8c563e5ce98
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-08 23:11:51 +0530

    array-3 solved

---
+++ 1_Javascript/Assignments/Array-3/squareUp.js
@@ -0,0 +1,14 @@
+let subArray = [];
+  let j = n;
+  while (j > 0) {
+    subArray.push(0);
+    j--;
+  }
+
+  let output = [];
+
+  for (let i = n - 1; i >= 0; i--) {
+    subArray.splice(i, 1, n - i);
+    output = output.concat(subArray);
+  }
+  return output;
