cd81c34888a05b43b0a954997ade58a37471e312
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-08 23:10:48 +0530

    array-3 solved

---
+++ 1_Javascript/Assignments/Array-3/linearIn.js
@@ -0,0 +1,12 @@
+function linearIn(outer, inner){
+    let j = 0;
+  
+    for (let i = 0; i < outer.length; i++) {
+      if (inner[j] == outer[i]) {
+        j++;
+      }
+    }
+  
+    return j == inner.length;
+  
+  }
