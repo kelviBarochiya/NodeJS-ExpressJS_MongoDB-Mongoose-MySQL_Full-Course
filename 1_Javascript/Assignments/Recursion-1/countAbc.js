cb0eb30c97502108e1f161a736bbf19fe9b2adb5
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-09 21:43:52 +0530

    recursion-1 solved

---
+++ 1_Javascript/Assignments/Recursion-1/countAbc.js
@@ -0,0 +1,10 @@
+function countAbc(str){
+    if (str.length <= 2) {
+      return 0;
+    }
+  
+    if (str.substring(0, 3) === "abc" || str.substring(0, 3) === "aba"){
+      return 1 + countAbc(str.substring(2));
+    }
+    return countAbc(str.substring(1))
+ }
