8f1cda4edaeddfdf4cbad0568dc0268de958e9d5
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-09 21:40:20 +0530

    recursion-1 solved

---
+++ 1_Javascript/Assignments/Recursion-1/endX.js
@@ -0,0 +1,10 @@
+function endX(str){
+    if(str.length <= 1) {
+         return str;
+       }
+   
+      if (str.charAt(0) == "x"){
+        return endX(str.substring(1)) + "x";
+      }
+      return str.charAt(0) + endX(str.substring(1));
+  }
