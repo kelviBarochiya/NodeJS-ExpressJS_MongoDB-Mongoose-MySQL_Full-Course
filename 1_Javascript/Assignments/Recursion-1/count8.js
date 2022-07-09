3d6a7f333c40ada90ab34868c97043175804fc64
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-09 20:24:16 +0530

    recursion-1 solved

---
+++ 1_Javascript/Assignments/Recursion-1/count8.js
@@ -0,0 +1,14 @@
+function count8(n){
+    if(n == 0){
+         return 0;
+       }
+   
+       if(n % 10 == 8) {
+           if(parseInt(n/10) % 10 == 8) {
+              return 2 + count8(parseInt(n/10));
+           } else {
+             return 1 + count8(parseInt(n/10));
+           }
+       }
+       return count8(parseInt(n/10));
+  }
