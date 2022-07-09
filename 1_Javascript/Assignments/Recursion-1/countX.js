c69cea91f114078292a69355a82d8749450f449a
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-09 20:31:07 +0530

    recursion-1 solved

---
+++ 1_Javascript/Assignments/Recursion-1/countX.js
@@ -0,0 +1,10 @@
+function countX(str){
+    if(str.length == 0)
+           return 0;
+   
+       if(str.charAt(0) == 'x'){
+         return 1 + countX(str.substring(1));
+       } else {
+         return countX(str.substring(1));
+       }
+  }
