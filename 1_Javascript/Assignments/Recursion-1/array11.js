f28c11c10b1ff85fe727d6071a81a1905096d53f
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-09 20:46:05 +0530

    recursion-1 solved

---
+++ 1_Javascript/Assignments/Recursion-1/array11.js
@@ -0,0 +1,11 @@
+function array11(nums, i){
+    if (i >= nums.length){
+       return 0;
+     }
+   
+     if (nums[i] == 11){
+       return 1 + array11(nums, i+1);
+     } else {
+       return array11(nums, i+1);
+     }
+  }
