7b432900909f43ffa25679a44a1a77a0a566d7e2
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-09 20:38:08 +0530

    recursion-1 solved

---
+++ 1_Javascript/Assignments/Recursion-1/changeXY.js
@@ -0,0 +1,10 @@
+function changeXY(str){
+    if (str.length == 0){
+       return "";
+     }
+   
+     if (str.charAt(0) == "x"){
+       return "y" + changeXY(str.substring(1))
+     }
+     return str.charAt(0) + changeXY(str.substring(1));
+  }
