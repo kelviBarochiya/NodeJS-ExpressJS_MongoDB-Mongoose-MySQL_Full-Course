76e12549f17c9cbd7c39268c4acd9ab0c43abd0f
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-09 20:42:37 +0530

    recursion-1 solved

---
+++ 1_Javascript/Assignments/Recursion-1/noX.js
@@ -0,0 +1,11 @@
+function noX(str){
+    if (str.length == 0){
+       return "";
+     }
+   
+     if (str.charAt(0) == "x"){
+       return noX(str.substring(1));
+     } else {
+       return str.charAt(0) + noX(str.substring(1));
+     }
+  }
