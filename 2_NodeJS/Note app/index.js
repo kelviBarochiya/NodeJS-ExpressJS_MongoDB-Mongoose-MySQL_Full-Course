e03f2ca1b4ad1a5d65c9c710f04fa5bffc43dbd9
kelviBarochiya <kelvibarochiya@gmail.com>
2022-07-17 22:04:54 +0530

    project

--- 2_NodeJS/Note app/index.js
+++ 2_NodeJS/Note app/index.js
@@ -1,25 +1,25 @@
 //synchronous
 
-//const fs = require("fs"); 
+const fs = require("fs"); 
 
-//fs.mkdirSync("app"); 
-
-
-//fs.writeFileSync('app/bio.txt', 'Hello world!');    
-
-//fs.appendFileSync('app/bio.txt', 'How are you!');     
+fs.mkdirSync("app"); 
 
 
-//const data = fs.readFileSync('app/bio.txt','utf8');
-//console.log(data);     
+fs.writeFileSync('app/bio.txt', 'Hello world!');    
+
+fs.appendFileSync('app/bio.txt', 'How are you!');     
 
 
-
-//fs.renameSync('app/bio.txt','app/myBio.txt');
+const data = fs.readFileSync('app/bio.txt','utf8');
+console.log(data);     
 
 
 
-//fs.unlinkSync('app/myBio.txt');
+fs.renameSync('app/bio.txt','app/myBio.txt');
+
+
+
+fs.unlinkSync('app/myBio.txt');
 
 
 
@@ -31,13 +31,13 @@ fs.writeFile("Newfile.txt","sample data" ,(err) =>{
 console.log("created");});
 
 
-//fs.appendFile("Newfile.txt","sample data changed" ,(err) =>{
-   // console.log("appended");});
+fs.appendFile("Newfile.txt","sample data changed" ,(err) =>{
+   console.log("appended");});
 
 
-//fs.readFile("Newfile.txt",'utf-8',(err,data) =>{
-   // console.log(data);});
+fs.readFile("Newfile.txt",'utf-8',(err,data) =>{
+   console.log(data);});
 
 
-//fs.unlink("Newfile.txt",(err) =>{
-        //console.log("delted");});  
+fs.unlink("Newfile.txt",(err) =>{
+   console.log("delted");});  
